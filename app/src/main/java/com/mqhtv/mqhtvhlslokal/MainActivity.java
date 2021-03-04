9e5099c08bce77058d2e4aac27a3873a80dd48ba
MQHTV Studio <ahmadisaputraindra@gmail.com>
2021-03-05 06:42:16 +0800

    sudah dibersihkan, ganti juga link ke lokal

--- app/src/main/java/com/mqhtv/mqhtvhlslokal/MainActivity.java
+++ app/src/main/java/com/mqhtv/mqhtvhlslokal/MainActivity.java
@@ -2,9 +2,11 @@ package com.mqhtv.mqhtvhlslokal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -12,34 +14,79 @@ import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
     SimpleExoPlayer player;
+    PlayerView playerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
-//        MediaItem mediaItem = MediaItem.fromUri(hlsUri);
-        String alamathlsmqhtv = "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8";
+    private void inisialisasi() {
+        //        MediaItem mediaItem = MediaItem.fromUri(hlsUri);
+//        String alamathlsmqhtv = "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8";
+        String alamathlsmqhtv = "192.168.100.14:8080/hls/mqhtv.m3u8";
         DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory();
         HlsMediaSource hlsMediaSource = new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(alamathlsmqhtv));
 
         player = new SimpleExoPlayer.Builder(this).build();
-        PlayerView playerView = (PlayerView) findViewById(R.id.playerView);
+        playerView = (PlayerView) findViewById(R.id.playerView);
         playerView.setPlayer(player);
 
         player.setMediaSource(hlsMediaSource);
+        player.setPlayWhenReady(true);
         player.prepare();
         player.play();
     }
 
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 
     @Override
-    protected void onStop() {
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24) {
+            inisialisasi();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT < 24 || player == null)) {
+            inisialisasi();
+        }
+    }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT < 24) {
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public void onStop() {
         super.onStop();
-//        Log.i("SUDAH KELUAR", "EXOPLAYER SUDAH DITUTUP");
-        player.release();
+        if (Util.SDK_INT >= 24) {
+            player.release();
+            player = null;
+        }
     }
 }
