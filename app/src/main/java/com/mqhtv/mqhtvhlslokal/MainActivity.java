957a66231a82dee3b5ce6025a351d09423e3b36f
MQHTV Studio <ahmadisaputraindra@gmail.com>
2021-03-05 09:49:17 +0800

    siap rilis v0.1

--- app/src/main/java/com/mqhtv/mqhtvhlslokal/MainActivity.java
+++ app/src/main/java/com/mqhtv/mqhtvhlslokal/MainActivity.java
@@ -29,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private void inisialisasi() {
         //        MediaItem mediaItem = MediaItem.fromUri(hlsUri);
 //        String alamathlsmqhtv = "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8";
-        String alamathlsmqhtv = "192.168.100.14:8080/hls/mqhtv.m3u8";
+//        String alamathlsmqhtv = "192.168.100.14:8080/hls/mqhtv.m3u8";
+        String alamathlsmqhtv = "http://192.168.1.9/hls/mqhtv.m3u8";
+
         DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory();
         HlsMediaSource hlsMediaSource = new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(alamathlsmqhtv));
 
