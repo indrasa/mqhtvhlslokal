b4ea51fb5d2c628fd93d4aee3cd95dd3059670da
MQHTV Studio <ahmadisaputraindra@gmail.com>
2021-03-06 10:57:32 +0800

    fix error dua, buang systemui, bersihkan dengan cara manual

--- app/src/main/java/com/mqhtv/mqhtvhlslokal/MainActivity.java
+++ app/src/main/java/com/mqhtv/mqhtvhlslokal/MainActivity.java
@@ -48,56 +48,57 @@ public class MainActivity extends AppCompatActivity {
         player.play();
     }
 
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
+//    @SuppressLint("InlinedApi")
+//    private void hideSystemUi() {
+//        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//    }
 
     @Override
     public void onStart() {
         super.onStart();
 //        if (Util.SDK_INT >= 24) {
+//            hideSystemUi();
             inisialisasi();
 //        }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        player.release();
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        player.release();
+//    }
+
+        @Override
+    public void onResume() {
+        super.onResume();
+//        hideSystemUi();
+        if ((Util.SDK_INT < 24 || player == null)) {
+            inisialisasi();
+        }
     }
 
-    //    @Override
-//    public void onResume() {
-//        super.onResume();
-//        hideSystemUi();
-//        if ((Util.SDK_INT < 24 || player == null)) {
-//            inisialisasi();
-//        }
-//    }
-//
-//
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        if (Util.SDK_INT < 24) {
-//            player.release();
-//            player = null;
-//        }
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        if (Util.SDK_INT >= 24) {
-//            player.release();
-//            player = null;
-//        }
-//    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT < 24) {
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT >= 24) {
+            player.release();
+            player = null;
+        }
+    }
 }
